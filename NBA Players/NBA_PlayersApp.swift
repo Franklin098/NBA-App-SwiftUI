34f420a96f6d048efe894a9daf892774ac8d7089
Franklin Velasquez <iefranklin@hotmail.es>
2023-03-25 21:12:14 -0600

    Initial Commit

---
+++ NBA Players/NBA_PlayersApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBA_PlayersApp.swift
+//  NBA Players
+//
+//  Created by Franklin Velásquez on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NBA_PlayersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
